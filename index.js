e072eaf68a75ef421f252d7c755d61f99b9e2fab
christian-gerard <1@christiangerard.me>
2024-01-30 21:35:54 -0700

    FINAL

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter((x) => x.toLowerCase() === string.toLowerCase())
+}
+
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter((x) => x.startsWith(string))
+
+}
+
+function matchName(drivers, string) {
+    return drivers.filter((x) => x.name === string)
+}
